06ffc7348b5900c06db77a060f51312862d8f71c
eigroup834 <eisocial103@gmail.com>
2025-12-05 13:37:31 +0530

    company search

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -521,50 +521,77 @@ exports.GetCompanyDetail = async (req, res) => {
       .input("COMPANY_CODE", sql.VarChar(50), companyCode)
       .query(`
         SELECT 
-          m.COMPANY_CODE, 
-          d.COMPANY_NAME, 
-          d.DIVISION, 
-          d.OLDNAME, 
-          d.ADDRESS, 
-          d.CITY, 
-          d.STATE, 
-          d.COUNTRY, 
+          d.COMPANY_CODE,
+          d.COMPANY_NAME,
+          d.DIVISION,
+          d.OLDNAME,
+          d.ADDRESS,
+          d.CITY,
+          d.STATE,
+          d.COUNTRY,
           d.PINCODE,
-          d.PHONES, 
-          d.EMAIL, 
-          d.WEBSITE, 
-          ds.SOURCE_CODE, 
-          ds.SOURCE_PERSON, 
+          d.PHONES,
+          d.EMAIL,
+          d.WEBSITE,
+          ds.SOURCE_CODE,
+          ds.SOURCE_PERSON,
           ds.SOURCE_TYPE,
-          m.REMARKS, 
+          m.REMARKS,
           m.MANAGEMENT_REMARKS,
-          -- Aggregate segments per industry
-          i.INDUSTRY,
-          STRING_AGG(s.SEG_CODE, ',') AS SEG_CODES,
-          STRING_AGG(s.SEGMENT, ',') AS SEGMENTS
-        FROM dbo.[${TABLES.COMP_MASTER}] m
-        LEFT JOIN dbo.[${TABLES.COMPANY_DETAIL}] d ON m.COMPANY_CODE = d.COMPANY_CODE
-        LEFT JOIN dbo.[${TABLES.COMP_SEGMENT_MAP}] s ON m.COMPANY_CODE = s.COMPANY_CODE
-        LEFT JOIN dbo.[${TABLES.DATA_SOURCE}] ds ON m.COMPANY_CODE = ds.COMPANY_CODE
-        LEFT JOIN dbo.[${TABLES.INDSEGMENT}] i ON s.SEG_CODE = i.SEG_CODE
-        WHERE m.COMPANY_CODE = @COMPANY_CODE
-        GROUP BY 
-          m.COMPANY_CODE, d.COMPANY_NAME, d.DIVISION, d.OLDNAME, d.ADDRESS, d.CITY, d.STATE, d.COUNTRY, d.PINCODE,
-          d.PHONES, d.EMAIL, d.WEBSITE, ds.SOURCE_CODE, ds.SOURCE_PERSON, ds.SOURCE_TYPE,
-          m.REMARKS, m.MANAGEMENT_REMARKS, i.INDUSTRY
+
+          -- INDUSTRIES
+          (
+            SELECT STRING_AGG(INDUSTRY, ',')
+            FROM (
+              SELECT DISTINCT i.INDUSTRY
+              FROM dbo.${TABLES.COMP_SEGMENT_MAP} s
+              JOIN dbo.${TABLES.INDSEGMENT} i ON s.SEG_CODE = i.SEG_CODE
+              WHERE s.COMPANY_CODE = m.COMPANY_CODE
+            ) x
+          ) AS INDUSTRY,
+
+          -- SEGMENT CODES
+          (
+            SELECT STRING_AGG(SEG_CODE, ',')
+            FROM (
+              SELECT DISTINCT s.SEG_CODE
+              FROM dbo.${TABLES.COMP_SEGMENT_MAP} s
+              WHERE s.COMPANY_CODE = m.COMPANY_CODE
+            ) x
+          ) AS SEG_CODES,
+
+          -- SEGMENT NAMES
+          (
+            SELECT STRING_AGG(SEGMENT, ',')
+            FROM (
+              SELECT DISTINCT i.SEGMENT
+              FROM dbo.${TABLES.COMP_SEGMENT_MAP} s
+              JOIN dbo.${TABLES.INDSEGMENT} i ON s.SEG_CODE = i.SEG_CODE
+              WHERE s.COMPANY_CODE = m.COMPANY_CODE
+            ) x
+          ) AS SEGMENTS
+
+        FROM dbo.${TABLES.COMP_MASTER} m
+        LEFT JOIN dbo.${TABLES.COMPANY_DETAIL} d 
+          ON m.COMPANY_CODE = d.COMPANY_CODE
+        LEFT JOIN dbo.${TABLES.DATA_SOURCE} ds 
+          ON m.COMPANY_CODE = ds.COMPANY_CODE
+
+        WHERE m.COMPANY_CODE = @COMPANY_CODE;
       `);
 
     if (result.recordset.length === 0) {
       return res.status(404).json({ success: false, message: "Company not found" });
     }
 
-    const data = result.recordset.map(row => ({
-      ...row,
-      SEG_CODES: row.SEG_CODES ? row.SEG_CODES.split(',') : [],
-      SEGMENTS: row.SEGMENTS ? row.SEGMENTS.split(',') : [],
-    }));
+    const row = result.recordset[0];
 
-    res.status(200).json(data);
+    res.status(200).json({
+      ...row,
+      INDUSTRY: row.INDUSTRY ? row.INDUSTRY.split(",") : [],
+      SEG_CODES: row.SEG_CODES ? row.SEG_CODES.split(",") : [],
+      SEGMENTS: row.SEGMENTS ? row.SEGMENTS.split(",") : [],
+    });
 
   } catch (err) {
     console.error("Error fetching company details:", err);
